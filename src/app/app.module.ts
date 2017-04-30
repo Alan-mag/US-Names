7c4d635dcaaa828bd409e832cf4ac218815dc018
Alan Magelssen <alanmagelssen@gmail.com>
2017-04-30 11:19:08 -0700

    add materialze

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Import Material Design
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+
+import 'hammerjs';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -11,6 +17,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule, // material imports
+    MdButtonModule, 
+    MdCheckboxModule,
     FormsModule,
     HttpModule
   ],
