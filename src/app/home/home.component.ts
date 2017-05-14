c7356b424bb98824be1e3fb3e88d56396dc63f35
Alan Magelssen <alanmagelssen@gmail.com>
2017-05-13 22:12:28 -0700

    update db search

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var localDatabase: any;
 
 @Component({
   selector: 'app-home',
@@ -21,13 +22,54 @@ export class HomeComponent implements OnInit {
 
   submitSearch(name: string, gender: string) {
     // function for searching through db and finding match
-    // if match not found, display 'no match'
-    // else, call function to gather and format data
+    // for each object in localdb array
+    // if name matches name && gender matches gender first letter
+    // console.log(thatobject)
+    // call function to gather and format data
     // send data to graph component
+
+    // parsing string format for values
+    // input name formatting
+    var firstLetter = name.charAt(0).toUpperCase();
+    var restOfName = name.substring(1).toLowerCase();
+    var formattedName = firstLetter + restOfName;
+    // throwing error after search execution
+
+    let myPromise = new Promise((resolve, reject) => {
+      let match: any;
+      localDatabase.forEach(element => {
+        if (formattedName == element.Name && gender.charAt(0) == element.Gender) {
+          match = element;
+          // parse data and format to send to graph component
+          console.log(element.Rank.split(" "));
+          resolve(match);
+        }
+      });
+
+      if (!match)
+        reject("No match for name/gender");
+    });
+    
+    
+    
+    myPromise.then((res) => {
+      if (res)
+        console.log(res);
+    });
+
+    myPromise.catch((fromReject) => {
+       console.log(fromReject);
+     });
+
   }
 
+  // search(name: string, gender: string) {
+  //   localDatabase.forEach(element => {
+  //       if (name == element.Name && gender.charAt(0) == element.Gender) {
+  //         var match = element;
+  //         return match;
+  //       }
+  //     });
+  // }
   // gather and format data function
-
-  // display error function
-
 }
