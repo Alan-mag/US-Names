d7692d0801f835d137a9799941e21dd3a768ffb0
Alan Magelssen <alanmagelssen@gmail.com>
2017-05-14 22:51:05 -0700

    update graph component

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef,OnChanges } from '@angular/core';
 import { D3Service, D3, Selection } from 'd3-ng2-service';
 
 
@@ -9,7 +9,7 @@ import { D3Service, D3, Selection } from 'd3-ng2-service';
 })
 
 export class GraphComponent implements OnInit {
-  @Input('rank') nameRank: string;
+  @Input('rank') nameRank: any;
 
   private d3: D3;
   private parentNativeElement: any;
@@ -26,6 +26,7 @@ export class GraphComponent implements OnInit {
     let d3 = this.d3;
     let d3ParentElement: Selection<any, any, any, any>;
 
+    /*
     if (this.parentNativeElement != null) {
       d3ParentElement = d3.select(this.parentNativeElement);
 
@@ -61,11 +62,59 @@ export class GraphComponent implements OnInit {
       .attr("y", function(d) { return y(d) + 3; })
       .attr("dy", ".75em")
       .text(function(d) { return d; });
-    }
+    }*/
   }
 
-  makeGraph() {         // call this function once data is sent and received
+  makeGraph(rankArray) {         // call this function once data is sent and received
 
+    let d3 = this.d3;
+    let d3ParentElement: Selection<any, any, any, any>;
+    
+
+    if (this.parentNativeElement != null) {
+      d3ParentElement = d3.select(this.parentNativeElement);
+
+      
+    let data = rankArray;
+
+    var margin = {top: 20, right: 30, bottom: 30, left: 40},
+        width = 864 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(data)])
+        .range([height, 0]);
+
+    var chart = d3.select(".chart")
+        .attr("width", width)
+        .attr("height", height);
+
+    var barWidth = width / data.length;
+
+    var bar = chart.selectAll("g")
+        .data(data)
+      .enter().append("g")
+        .attr("transform", function(d, i) { return "translate(" + i * barWidth + ")"; });
+
+    bar.append("rect")
+      .attr("y", function(d) { return y(d); })
+      .attr("height", function(d) { return height - y(d); })
+      .attr("width", barWidth - 1);
+
+    bar.append("text")
+        .attr("x", barWidth / 2)
+        .attr("y", function(d) { return y(d) + 3; })
+        .attr("dy", ".75em")
+        .text(function(d) { return d; });
+      }
+  }
+
+  ngOnChanges(nameRank: string) {
+    console.log(this.nameRank);
+    var arrayOfRanks = this.nameRank.split(" ");
+    console.log(arrayOfRanks);
+    makeGraph(arrayOfRanks);
+    return nameRank;
   }
 
   // need function to gather data and use with d3
